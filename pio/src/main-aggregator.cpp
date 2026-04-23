eedfe0b443de1182fbd6034c68de3b5fa38a8efb
pacjo <korycinski.kam@gmail.com>
2026-04-23 08:46:13 +0200

    remove unused, rename

--- pio/src/main-aggregator.cpp
+++ pio/src/main-aggregator.cpp
@@ -1,27 +1,221 @@
 #include <Arduino.h>
+#include <CRC.h>
+#include <cstdint>
+#include <sys/_intsup.h>
+#include "api/Common.h"
+#include "led_helper.h"
 #include "uart_helper.h"
 #include "const_defs.h"
-char cmd;
+#include "proto_defs.h"
+
+#define MY_ID W0_ID
+
+#define SERIAL1_NEI W3_ID
+#define SERIAL4_NEI W4_ID
+
+#define TIMEOUT_TOTAL 10000
+
+byte Tx_buff[BUFF_LEN], Rx_buff[BUFF_LEN];
+byte ret_buf[RET_LEN];
+byte choice = '0';
+
+uint8_t awaiting_res1, awaiting_res4 = 0;
+unsigned long sent1, sent4 = 0;
+
+void handleLHS(Uart &from, uint8_t from_id) {
+    uint8_t recv = from.available();
+    uint8_t crc_valid = 0;
+    uint8_t id = 0;
+    uint8_t r, g, b, maxVal;
+    r = g = b = maxVal = 0;
+
+    if (!(recv >= RET_LEN))
+        return;
+
+    setLedState(RECEIVING);
+
+    from.readBytes(Rx_buff, min(BUFF_LEN, recv));
+
+    crc_valid = GET_CRC(Rx_buff) == calcCRC8(Rx_buff, recv - 1);
+
+    if (!crc_valid) {
+        SET_DST(ret_buf, from_id);
+        SET_CRC(ret_buf, calcCRC8(ret_buf, RET_LEN - 1));
+        from.write(ret_buf, RET_LEN);
+        return;
+    }
+
+    // Spodziewamy się RET, DEAD, RES
+    switch (GET_TYP(Rx_buff)) {
+        case MSG_T_RET:
+        setLedState(RETRANSMITTING);
+        from.write(Tx_buff, REQ_LEN);
+        break;
+        case MSG_T_DEAD:
+        setLedState(GENERIC_ERROR);
+        Serial.print(F("Wykryto awarię łącza pomiędzy "));
+        switch (GET_SRC(Rx_buff)) {
+            case W1_ID: Serial.print(F("W1 i ")); break;
+            case W2_ID: Serial.print(F("W2 i ")); break;
+            case W3_ID: Serial.print(F("W3 i ")); break;
+            case W4_ID: Serial.print(F("W4 i ")); break;
+            default: Serial.print(F("N/A"));
+        }
+        switch (GET_DEAD(Rx_buff)) {
+            case W1_ID: Serial.print(F("W1.")); break;
+            case W2_ID: Serial.print(F("W2.")); break;
+            case S1_ID: Serial.print(F("S1.")); break;
+            case S2_ID: Serial.print(F("S2.")); break;
+            case S3_ID: Serial.print(F("S3.")); break;
+            case S4_ID: Serial.print(F("S4.")); break;
+            case S5_ID: Serial.print(F("S5.")); break;
+            default: Serial.print(F("N/A."));
+        }
+
+        break;
+        case MSG_T_RES:
+        setLedState(RECEIVING);
+        if (from_id == SERIAL1_NEI)
+            awaiting_res1 = 0;
+        else if (from_id == SERIAL4_NEI)
+            awaiting_res4 = 0;
+        // else
+        //     Serial.println(F("Coś jest grubo nie tak"));
+
+        id = GET_SRC(Rx_buff);
+        switch (id) {
+            case S1_ID:
+            UNPACK_RGB(GET_READ(Rx_buff), r, g, b);
+            Serial.print(F("Otrzymano odczyt z S1: R="));
+            Serial.print(r, DEC);
+            Serial.print(F(", G="));
+            Serial.print(g, DEC);
+            Serial.print(F(", B="));
+            Serial.print(b, DEC);
+            Serial.print(F(". Dominujący kolor: "));
+            maxVal = max(r, max(g, b));
+            if (maxVal == r)
+                Serial.println(F("czerwony."));
+            else if (maxVal == g)
+                Serial.println(F("zielony."));
+            else if (maxVal == b)
+                Serial.println(F("niebieski."));
+            else
+                Serial.println(F("N/A."));
+
+            break;
+            default:
+            Serial.print(F("Otrzymano ID "));
+            Serial.print(id, DEC);
+            Serial.print(F(" od węzła "));
+            switch (id) {
+                case S2_ID: Serial.print(F("S2.")); break;
+                case S3_ID: Serial.print(F("S3.")); break;
+                case S4_ID: Serial.print(F("S4.")); break;
+                case S5_ID: Serial.print(F("S5.")); break;
+                default: Serial.print(F("N/A."));
+            }
+        }
+
+        break;
+        default:
+        Serial.print(F("Otrzymano nieznany typ wiadomości: 0x"));
+        Serial.println(GET_TYP(Rx_buff), HEX);
+    }
+}
 
 void setup() {
     Serial.begin(115200);
-    while (!Serial);
 
-    // sensor nodes
     Serial1.begin(UART_BAUD);
+    Serial4.begin(UART_BAUD);
 
     setup_sercoms();
+    memset((void*) Tx_buff, 0, BUFF_LEN);
+    memset((void*) Rx_buff, 0, BUFF_LEN);
+    memset((void*) ret_buf, 0, RET_LEN);
+    SET_TYP(ret_buf, MSG_T_RET);
 }
 
 void loop() {
-    if (Serial.available()) {
+    setLedState(IDLE);
 
-        Serial.readBytes(&cmd, 1);
-        Serial1.write(cmd);
+    // Dla pewności wyzerowanie wyboru
+    choice = '0';
+    unsigned long time1 = millis() - sent1,
+    time4 = millis() - sent4;
+
+    if ((awaiting_res1 || awaiting_res4) &&
+        (time1 <= TIMEOUT_TOTAL ||
+         time4 <= TIMEOUT_TOTAL ))
+            goto await;
+
+    // Upłynął timeout i nie mamy odpowiedzi
+    if (awaiting_res1 && time1 > TIMEOUT_TOTAL) {
+        Serial.print(F("Nie otrzymano odpowiedzi od W3 w ciągu "));
+        Serial.print(TIMEOUT_TOTAL/1000, DEC);
+        Serial.println(F(" sekund"));
+        awaiting_res1 = 0;
+        return;
     }
 
-    if (Serial1.available()) {
-        Serial1.readBytes(&cmd, 1);
-        Serial.write(cmd);
+    if (awaiting_res4 && time4 > TIMEOUT_TOTAL) {
+        Serial.print(F("Nie otrzymano odpowiedzi od W4 w ciągu "));
+        Serial.print(TIMEOUT_TOTAL/1000, DEC);
+        Serial.println(F(" sekund"));
+        awaiting_res4 = 0;
+        return;
     }
+
+    // Wyczyszczenie bufora Rx
+    while (Serial.available())
+        Serial.read();
+
+    Serial.print(F(MAIN_MENU_TEXT));
+
+    if (!Serial.available())
+        return;
+
+    // Odczytanie odpowiedzi użytkownika
+    choice = Serial.read();
+
+    // zmiana wyświetlanego stanu
+    setLedState(TRANSMITTING);
+
+    // Przygotowanie bazowego REQ
+    memset(Tx_buff, 0, BUFF_LEN);
+    SET_TYP(Tx_buff, MSG_T_REQ);
+
+    // Ustawienie DST i oczekiwania na odpowiedź
+    switch (choice) {
+        case '6': SET_CMD(Tx_buff, 1);
+        case '1': SET_DST(Tx_buff, S1_ID); awaiting_res1 = awaiting_res4 = 1 | (S1_ID << 1); break;
+        case '2': SET_DST(Tx_buff, S2_ID); awaiting_res1 = awaiting_res4 = 1 | (S2_ID << 1); break;
+        case '3': SET_DST(Tx_buff, S3_ID); awaiting_res1 = awaiting_res4 = 1 | (S3_ID << 1); break;
+        case '4': SET_DST(Tx_buff, S4_ID); awaiting_res1 = awaiting_res4 = 1 | (S4_ID << 1); break;
+        case '5': SET_DST(Tx_buff, S5_ID); awaiting_res1 = awaiting_res4 = 1 | (S5_ID << 1); break;
+        default: Serial.print(F("Nieprawidłowy wybór: ")); Serial.println(choice); return;
+    }
+
+    SET_CRC(Tx_buff, calcCRC8(Tx_buff, REQ_LEN - 1));
+
+    // Wysłanie REQ i zapisanie czasu wysłania
+    Serial1.write(Tx_buff, REQ_LEN);
+    sent1 = millis();
+    Serial4.write(Tx_buff, REQ_LEN);
+    sent4 = millis();
+
+    // Zapewnienie użytkownika
+    Serial.print(F("Wysłano zapytanie, oczekiwanie na odpowiedź przez "));
+    Serial.print(TIMEOUT_TOTAL/1000, DEC);
+    Serial.println(F(" sekund"));
+
+    // zmiana wyświetlanego ponownie na IDLE, zaraz konkretny da handleLHS
+    setLedState(IDLE);
+
+    // Oczekiwanie na to co wróci
+    await:
+    handleLHS(Serial1, SERIAL1_NEI);
+    handleLHS(Serial4, SERIAL4_NEI);
+
 }
--- pio/src/main-sensor.cpp
+++ pio/src/main-sensor.cpp
@@ -1,57 +1,90 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include "color_helper.h"
+#include "led_helper.h"
+#include "uart_helper.h"
 #include "const_defs.h"
-/* #TODO: Przerobić to na coś co korzysta z protokołu i UART'a (trzeba jeszcze przekonwertować odczyty z 8 na 4 bity) */
-// ================== KONFIG ==================
-#define DEVICE_ADDR 0x10   // ZMIENIASZ dla każdego czujnika
-// ===========================================
+#include "proto_defs.h"
+#include "color_helper.h"
+
+#define MY_ID S1_ID
+
+#if MY_ID==S2_ID || MY_ID==S3_ID
+#define MASTER_ID W2_ID
+#elif MY_ID==S1_ID || MY_ID==S4_ID || MY_ID==S5_ID
+#define MASTER_ID W1_ID
+#endif
+
+byte Tx_buff[BUFF_LEN], Rx_buff[BUFF_LEN];
+byte ret_msg[RET_LEN];
 
 uint8_t maxVals[5] = {0};
 
-volatile bool requestFlag = false;
-
-// Funkcja wywoływana gdy master chce dane
-void onRequest() {
-  uint8_t data[4];
-
-  data[0] = DEVICE_ADDR;
-  data[1] = readColor(COLOR_RED);   // TODO: take more then RED
-  data[2] = millis() & 0xFF;
-  data[3] = 0xAA;
-
-  Wire.write(data, 4);
-}
-
-// Opcjonalnie: odbiór danych od mastera
-void onReceive(int len) {
-  while (Wire.available()) {
-    uint8_t cmd = Wire.read();
-    Serial.print("CMD: ");
-    Serial.println(cmd);
-  }
-}
-
 void setup() {
-  Serial.begin(115200);
+    Serial1.begin(UART_BAUD);
+    setup_sercoms();
 
-  // TODO: move to color sensor setup function
-  pinMode(S0, OUTPUT);
-  pinMode(S1, OUTPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
-  pinMode(OUT_PIN, INPUT);
-  digitalWrite(S0, HIGH);
-  digitalWrite(S1, LOW);  // skala 20%
+    memset((void*) Tx_buff, 0, BUFF_LEN);
+    memset((void*) Rx_buff, 0, BUFF_LEN);
 
-  Wire.begin(DEVICE_ADDR); // SLAVE
-  Wire.onRequest(onRequest);
-  Wire.onReceive(onReceive);
-
-  Serial.print("SLAVE START addr=0x");
-  Serial.println(DEVICE_ADDR, HEX);
+    SET_DST(ret_msg, MASTER_ID);
+    SET_TYP(ret_msg, MSG_T_RET);
+    SET_CRC(ret_msg, calcCRC8(ret_msg, RET_LEN - 1));
+    setLedState(IDLE);
 }
 
 void loop() {
-  delay(100);
+    updateLedTask();
+
+    while (Serial1.available() > 0) {
+        uint8_t typ = (Serial1.peek() & 0x0E) >> 1;
+        uint8_t exp_len = 0;
+
+        if (typ == MSG_T_REQ) exp_len = REQ_LEN;
+        else if (typ == MSG_T_RES) exp_len = RES_LEN;
+        else if (typ == MSG_T_DEAD) exp_len = DEAD_LEN;
+        else if (typ == MSG_T_RET) exp_len = RET_LEN;
+        else { Serial1.read(); continue; }
+
+        if (Serial1.available() < exp_len) break; // Zbyt mało bajtów
+
+        triggerLedState(RECEIVING);
+        memset((void*) Rx_buff, 0, BUFF_LEN);
+        Serial1.readBytes(Rx_buff, exp_len);
+
+        if (GET_CRC(Rx_buff) != calcCRC8(Rx_buff, exp_len - 1)) {
+            while(Serial1.available()) Serial1.read();
+            Serial1.write(ret_msg, RET_LEN);
+            triggerLedState(GENERIC_ERROR);
+            break;
+        }
+
+        switch(GET_TYP(Rx_buff)) {
+            case MSG_T_REQ: {
+                if (GET_DST(Rx_buff) != MY_ID) continue; // Ramka OK, ale nie do nas (ignoruj)
+
+                triggerLedState(TRANSMITTING);
+                memset((void*) Rx_buff, 0, BUFF_LEN);
+
+                SET_DST(Tx_buff, W0_ID);
+                SET_TYP(Tx_buff, MSG_T_RES);
+                SET_SRC(Tx_buff, MY_ID);
+
+                uint16_t payload =
+                ((readColor(COLOR_RED) >> 4) << 8) |
+                ((readColor(COLOR_GREEN) >> 4) << 4) |
+                (readColor(COLOR_BLUE) >> 4);
+                SET_READ(Tx_buff, payload);
+
+                SET_CRC(Tx_buff, calcCRC8(Tx_buff, RES_LEN - 1));
+                Serial1.write(Tx_buff, RES_LEN);
+                break;
+            }
+            case MSG_T_RET: {
+                if( GET_DST(Rx_buff) == MY_ID ) {
+                    Serial1.write(Tx_buff, RES_LEN);
+                    triggerLedState(RETRANSMITTING);
+                }
+                break;
+            }
+        }
+    }
 }
